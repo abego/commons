c2bb00d3e7f213dab907c2074706d75e41637274
Udo Borkowski <ub@abego-software.de>
2020-10-04 14:47:03 +0200

    New: add SPI marker annotation

---
+++ abego-commons-base/src/main/java/org/abego/commons/annotation/SPI.java
@@ -0,0 +1,57 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2020 Udo Borkowski, (ub@abego.org)
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+package org.abego.commons.annotation;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * SPI marker annotation.
+ * <p>
+ * The annotated element is (part of) an SPI (Service Provider Interface).
+ * An SPI is an element designed to be extended or implemented by client code.
+ * <p>
+ * In contrast to an SPI an API <em>(*)</em> is an element to be used and
+ * called by client code (and <em>not</em> to be extended or implemented).
+ * <p>
+ * <em>(Client code refers to code outside of the module that references
+ * the module.)</em>
+ * <p>
+ * <em>Notice: the term "API" is often used with two meanings:</em>
+ * <ul>
+ *     <li><em>The interface part of a module, i.e. all elements to be used,
+ *     called, extended or implemented by client code.</em>/li>
+ *     <li><em>The interface part of a module that is not its SPI.</em></li>
+ * </ul>
+ */
+@Documented
+@Retention(RetentionPolicy.RUNTIME)
+@Target({ElementType.TYPE, ElementType.PACKAGE})
+public @interface SPI {
+    boolean value() default true;
+}

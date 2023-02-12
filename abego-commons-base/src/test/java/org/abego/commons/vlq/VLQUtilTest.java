0b5836ffa41737f1bf9cbfbf1a164f6ac63a663e
Udo Borkowski <ub@abego-software.de>
2023-02-12 19:11:15 +0100

    new(VLQUtil): support signed int encoding and decoding

--- abego-commons-base/src/test/java/org/abego/commons/vlq/VLQUtilTest.java
+++ abego-commons-base/src/test/java/org/abego/commons/vlq/VLQUtilTest.java
@@ -1,7 +1,7 @@
 /*
  * MIT License
  *
- * Copyright (c) 2021 Udo Borkowski, (ub@abego.org)
+ * Copyright (c) 2022 Udo Borkowski, (ub@abego.org)
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal

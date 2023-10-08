e277d81c8ae899319962954c1ab38f59e7b8f39e
Udo Borkowski <ub@abego-software.de>
2023-10-08 17:53:26 +0200

    feat: add JUnit5Util methods
    - assertEqualLines
    - assertEqualFiles

--- abego-commons-test/src/main/java/org/abego/commons/test/JUnit5Util.java
+++ abego-commons-test/src/main/java/org/abego/commons/test/JUnit5Util.java
@@ -1,7 +1,7 @@
 /*
  * MIT License
  *
- * Copyright (c) 2020 Udo Borkowski, (ub@abego.org)
+ * Copyright (c) 2023 Udo Borkowski, (ub@abego.org)
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
@@ -27,12 +27,18 @@ package org.abego.commons.test;
 import org.abego.commons.io.FileUtil;
 import org.abego.commons.lang.exception.MustNotInstantiateException;
 import org.abego.commons.range.IntRange;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.function.Executable;
 
 import java.io.File;
 import java.nio.charset.Charset;
 import java.nio.charset.StandardCharsets;
 import java.text.MessageFormat;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 import static java.lang.String.format;
 import static org.abego.commons.lang.ClassUtil.classNameOrNull;
@@ -45,6 +51,16 @@ public class JUnit5Util {
 
     JUnit5Util() {
         throw new MustNotInstantiateException();
+    }
+
+    /**
+     * Asserts the actual String has the same lines as the expected String,
+     * ignoring possible difference is the line separators ("\n" vs "\r\n")
+     */
+    public static void assertEqualLines(String expected, String actual) {
+        String e = expected.replaceAll("\r", "");
+        String a = actual.replaceAll("\r", "");
+        assertEquals(e, a);
     }
 
     public static void assertTextContains(
@@ -101,4 +117,52 @@ public class JUnit5Util {
         return (T) object;
     }
 
+    /**
+     * Asserts the directories {@code expectedDir} and {@code actualDir}
+     * contain equal files, also comparing the content of matching regular files
+     * (assume text files with UTF-8 encoding).
+     */
+    public static void assertEqualFiles(File expectedDir, File actualDir) {
+        List<Executable> allAsserts = new ArrayList<>();
+        addAssertsComparingDirectories(allAsserts, expectedDir, actualDir, "");
+        Assertions.assertAll(allAsserts);
+    }
+
+    private static void addAssertsComparingDirectories(
+            List<Executable> allAsserts, File expectedDir, File actualDir, String prefix) {
+        Set<String> expectedFiles = Arrays.stream(FileUtil.filesInDirectory(expectedDir))
+                .map(File::getName).collect(Collectors.toSet());
+        Set<String> actualFiles = Arrays.stream(FileUtil.filesInDirectory(actualDir))
+                .map(File::getName).collect(Collectors.toSet());
+
+        allAsserts.add(() ->
+                assertEquals(expectedFiles.stream()
+                                .sorted()
+                                .map(s -> prefix + s)
+                                .collect(Collectors.joining("\n")),
+                        actualFiles.stream()
+                                .sorted()
+                                .map(s -> prefix + s)
+                                .collect(Collectors.joining("\n")), "Missing/Extra Files")
+        );
+
+        // work on all files that exist in both directories
+        expectedFiles.retainAll(actualFiles);
+        expectedFiles.stream().sorted().forEach(filename -> {
+            File expectedFile = new File(expectedDir, filename);
+            File actualFile = new File(actualDir, filename);
+            if (expectedFile.isDirectory() != actualFile.isDirectory()) {
+                allAsserts.add(() -> fail("Mixing directory and regular file: " + prefix + filename));
+            } else {
+                if (expectedFile.isDirectory()) {
+                    addAssertsComparingDirectories(allAsserts, expectedFile, actualFile, prefix + filename + "/");
+                } else {
+                    allAsserts.add(() ->
+                            assertEquals(org.abego.commons.io.FileUtil.textOf(expectedFile),
+                                    org.abego.commons.io.FileUtil.textOf(actualFile),
+                                    "Difference in file: " + prefix + filename));
+                }
+            }
+        });
+    }
 }
